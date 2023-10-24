4d2b8b424d68295ffae0fe4582266e80dcc1c209
brahian github <brahiangithub@gmail.com>
2023-10-24 15:49:47 -0500

    ejercicio prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loteria';
+
+  codigo = "";
+  respuesta1 = 0;
+  respuesta2 = 0;
+
+  ganadores = ['ganador1','ganador2','ganador3']
+  ganadorRenault = 'ganadorF'
+
+  ganador(){
+      if (this.ganadores.includes(this.codigo)){
+         this.respuesta1 = 1
+         this.respuesta2 = 0
+      }
+      else if(this.ganadorRenault == this.codigo){
+         this.respuesta2 = 1
+         this.respuesta1 = 0
+      }
+      else{
+         this.respuesta1 = 2
+         this.respuesta2 = 2
+      }
+      console.log(this.respuesta1);
+      console.log(this.respuesta2);
+
+  }
+
 }
